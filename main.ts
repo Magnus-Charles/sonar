2176e7b08fa537e0edf73138f47cd9da50eb9b12
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-02 12:41:56 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+// Created by: magnus
+// Created on: Oct 2020
+// 
+// This program is used to find distance with a sonar
+let distance_to_object = 0
+basic.showIcon(IconNames.Square)
+basic.pause(200)
 basic.forever(function () {
-	
+    distance_to_object = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P0,
+    PingUnit.MicroSeconds
+    )
+    basic.showNumber(distance_to_object)
 })
